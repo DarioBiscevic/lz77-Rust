8b1dc1167b0f48de51f8214d3320e50a67c91fec
Dario Biscevic <44779438+DarioBiscevic@users.noreply.github.com>
2020-07-16 19:19:54 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,19 @@ mod lib;
 use lib::lz_compress::*;
 
 fn main() {
-    println!("LZ77 implementation in Rust");
+    println!("LZ77 implementation in Rust\n");
 
     let input = String::from("This is a test. This test is made for testing");
     
-    
+    println!("Input:\n{}\n", input);
     println!("Input length: {}\n", input.as_bytes().len());
     
     let output = lz77_compress(&input);
 
-    println!("Output (bytes): {:?}", output);
-    println!("Output length: {}", output.len());
+    //println!("Output (bytes): {:?}", output);
+    println!("Compressed length: {}\n", output.len());
 
 
-    println!("\n\nDecompressed: {}", lz77_decompress(&output));
+    println!("Decompressed:\n{}", lz77_decompress(&output));
     
 }
